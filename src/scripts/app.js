04ab1f97142174965fc90858ac31b3960271b032
Guilherme Gules Moreira <guilhermegules@gmail.com>
2025-01-10 21:57:31 -0300

    feat: increase read speed with file.stream and add a better formatting and style

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,98 +2,104 @@ const filePicker = document.getElementById("file-picker");
 const content = document.getElementById("content");
 
 function highlightJSON(value, depth = 0) {
-  const indent = "&nbsp;".repeat(depth * 4);
-  const newLine = "<br/>";
+  const indentClass = `depth-${depth}`;
 
   if (Array.isArray(value)) {
-    const itens = value
+    const items = value
       .map(
         (item, index) =>
-          `${indent}&nbsp;&nbsp;&nbsp;<span class="index">${index}</span>: ${highlightJSON(
+          `<div class="json-itens ${indentClass}">
+            <span class="index">${index}:</span> ${highlightJSON(
             item,
             depth + 1
-          )}`
+          )}
+          </div>`
       )
-      .join(`${newLine}`);
-    return `<span class="bracket">[</span><span class="json-itens">${newLine}${itens}${newLine}${indent}</span><span class="bracket">]</span>`;
+      .join("");
+
+    return `
+      <span class="bracket">[</span>
+      <div class="${indentClass} json-content">${items}</div>
+      <span class="bracket">]</span>`;
   }
 
   if (value && typeof value === "object") {
-    const objectWithInnerObject = (item) =>
-      `<span class="bracket">{</span> ${highlightJSON(
-        item,
-        depth + 1
-      )}<span class="bracket">}</span>,`;
-
-    const showValue = (item) =>
-      typeof item === "object" && !Array.isArray(item)
-        ? objectWithInnerObject(item)
-        : highlightJSON(item, depth + 1);
-
     const entries = Object.entries(value)
       .map(
         ([key, item]) =>
-          `<span class="json-itens">
-            ${indent}&nbsp;&nbsp;&nbsp;&nbsp;<span class="key">"${key}"</span>:${showValue(
-            item
-          )} 
-          </span>`
+          `<div class="json-itens ${indentClass}">
+            <span class="key">"${key}"</span>: ${highlightJSON(item, depth + 1)}
+          </div>`
       )
-      .join(`${newLine}`);
-    return `<span class="json-itens">${newLine}${entries}${newLine}${indent}</span>`;
+      .join("");
+
+    return `
+      <span class="bracket">{</span>
+      <div class="${indentClass} json-content">${entries}</div>
+      <span class="bracket">}</span>`;
   }
 
   if (value === null) {
     return `<span class="value">null</span>`;
   }
 
-  if (value === "") {
-    return `<span class="value">""</span>`;
-  }
-
   if (typeof value === "number") {
     return `<span class="value">${value}</span>`;
   }
 
-  return `<span class="value">"${value}"</span>`;
+  if (typeof value === "string") {
+    return `<span class="value">"${value}"</span>`;
+  }
+
+  return `<span class="value">${value}</span>`;
 }
 
-filePicker.addEventListener("change", (e) => {
+function appendCollapseEvent() {
+  const collapseBrackets = document.querySelectorAll(".bracket");
+
+  collapseBrackets.forEach((bracket) => {
+    bracket.addEventListener("click", () => {
+      const isCollapsed =
+        bracket.nextElementSibling.classList.contains("collapsed");
+
+      if (isCollapsed) {
+        bracket.classList.remove("collapsed-content");
+        bracket.nextElementSibling.classList.remove("collapsed");
+        return;
+      }
+
+      bracket.nextElementSibling.classList.add("collapsed");
+      bracket.classList.add("collapsed-content");
+    });
+  });
+}
+
+filePicker.addEventListener("change", async (e) => {
   const [file] = e.target.files;
 
-  if (file) {
-    const reader = new FileReader();
-
-    reader.onload = (readerEvent) => {
-      try {
-        const jsonData = JSON.parse(readerEvent.target.result);
-        content.innerHTML = `<section><div class="json">${highlightJSON(
-          jsonData
-        )}</div></section>`;
-
-        const collapseBrackets = document.querySelectorAll(".bracket");
-
-        collapseBrackets.forEach((bracket) => {
-          bracket.addEventListener("click", (event) => {
-            const isCollapsed =
-              bracket.nextElementSibling.classList.contains("collapsed");
-
-            if (isCollapsed) {
-              bracket.classList.remove("collapsed-content");
-              bracket.nextElementSibling.classList.remove("collapsed");
-            } else {
-              bracket.nextElementSibling.classList.add("collapsed");
-              bracket.classList.add("collapsed-content");
-            }
-          });
-        });
-      } catch (error) {
-        console.error("Erro ao ler o arquivo JSON:", error);
-      }
-    };
-
-    reader.readAsText(file);
-  } else {
+  if (!file) {
     console.log("Nenhum arquivo selecionado.");
+    return;
   }
+
+  const stream = file.stream();
+  const reader = stream.getReader();
+  const decoder = new TextDecoder("utf-8");
+  let jsonString = "";
+
+  while (true) {
+    const { done, value } = await reader.read();
+
+    if (done) break;
+
+    jsonString += decoder.decode(value, { stream: true });
+  }
+
+  if (!jsonString) return;
+
+  content.innerHTML = `<section class="json-container"><div class="json">${highlightJSON(
+    JSON.parse(jsonString)
+  )}</div></section>`;
+
+  appendCollapseEvent();
 });
