9731edbd326a58bf7a9d5d859ffbb0ba1b36981c
Guilherme Gules Moreira <guilherme.moreira@e-unicred.com.br>
2025-01-17 15:09:44 -0300

    feat(web): add drop zone for json tree view

--- apps/web/src/scripts/json-tree-view.ts
+++ apps/web/src/scripts/json-tree-view.ts
@@ -1,7 +1,8 @@
 import { getJsonContainer } from "@repo/json-utils";
 
-const filePicker = document.getElementById("file-picker");
-const content = document.getElementById("content");
+const filePicker = document.getElementById("file-picker")!;
+const content = document.getElementById("content")!;
+const dropZone = document.getElementById("drop-zone")!;
 
 function appendCollapseEvent() {
   const collapseBrackets = document.querySelectorAll(".bracket");
@@ -23,8 +24,48 @@ function appendCollapseEvent() {
   });
 }
 
-filePicker!.addEventListener("change", async (e) => {
-  const [file] = (e.target as HTMLInputElement).files!;
+filePicker.addEventListener("change", async (e) => {
+  const files = (e.target as HTMLInputElement).files;
+  if (!files) return;
+  handleFiles(files);
+});
+
+dropZone.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  dropZone.classList.add("dragover");
+});
+
+dropZone.addEventListener("dragleave", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  dropZone.classList.remove("dragover");
+});
+
+dropZone.addEventListener("click", () => {
+  filePicker.click();
+});
+
+dropZone.addEventListener("drop", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+
+  dropZone.classList.remove("dragover");
+
+  const files = e.dataTransfer?.files;
+
+  if (!files || files.length === 0) {
+    console.log(
+      "No files were dropped. Make sure you're dragging files and not other items."
+    );
+    return;
+  }
+
+  handleFiles(files);
+});
+
+async function handleFiles(files: FileList) {
+  const [file] = files;
 
   if (!file) {
     console.log("Nenhum arquivo selecionado.");
@@ -46,7 +87,7 @@ filePicker!.addEventListener("change", async (e) => {
 
   if (!jsonString) return;
 
-  content!.innerHTML = getJsonContainer(JSON.parse(jsonString));
+  content.innerHTML = getJsonContainer(JSON.parse(jsonString));
 
   appendCollapseEvent();
-});
+}
